d31110c95e20f2fca39cab151e7b3d63493c46f0
priyankakarnati11@gmail.com <priyankakarnati11@gmail.com>
2020-03-22 20:49:27 +0530

    integrated retrofit with coroutines

--- app/src/main/java/com/example/gitapp/network/GitApiService.kt
+++ app/src/main/java/com/example/gitapp/network/GitApiService.kt
@@ -1,8 +1,9 @@
 package com.example.gitapp.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -12,12 +13,13 @@ private const val BASE_URL = "https://api.github.com/"
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
 private val retrofit =
-    Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(
+    Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory()).baseUrl(
     BASE_URL).build()
 interface GitApiService{
     @GET("repositories")
-    fun getProperties(@Query("since") since:Int):
-            Call<List<GitProperty>>
+    fun getPropertiesAsync(@Query("since") since: Int):
+            Deferred<List<GitProperty>>
 
 }
 
--- app/src/main/java/com/example/gitapp/vals/OverviewViewModel.kt
+++ app/src/main/java/com/example/gitapp/vals/OverviewViewModel.kt
@@ -4,16 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.gitapp.network.GitApi
-import com.example.gitapp.network.GitProperty
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import java.lang.Exception
 
 class OverviewViewModel:ViewModel() {
     private val _response = MutableLiveData<String>()//internal immutable string storing the most recent response
     val response : LiveData<String>//external mutable LiveData for response String
         get() = _response
+
+    private val viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
     init{
         getGitApiResponse()
@@ -21,22 +21,41 @@ class OverviewViewModel:ViewModel() {
     }
 
     private fun getGitApiResponse(){
+        coroutineScope.launch {
+            var getPropertiesDeferred = GitApi.retrofitService.getPropertiesAsync(5)
 
-        GitApi.retrofitService.getProperties(5).enqueue(object : Callback<List<GitProperty>> {
-            override fun onFailure(call: Call<List<GitProperty>>, t: Throwable) {
-                    _response.value =  "Failure: " + t.message
-                    Log.i("on Failed","failed")
-                }
-
-
-            override fun onResponse(
-                call: Call<List<GitProperty>>,
-                response: Response<List<GitProperty>>
-            ) {
-                _response.value = "Success :${response.body()?.size} git properties retrieved"
-                //Log.i("onSuccess",response.body())
+            try {
+                var listResult = getPropertiesDeferred.await()
+                _response.value = "Success : ${listResult.size} git properties retrieved."
+            } catch (e: Exception) {
+                _response.value = "Failed: ${e.message}"
             }
-        })
+
+
+        }
+
+//        GitApi.retrofitService.getProperties(5).enqueue(object : Callback<List<GitProperty>> {
+//            override fun onFailure(call: Call<List<GitProperty>>, t: Throwable) {
+//                    _response.value =  "Failure: " + t.message
+//                    Log.i("on Failed","failed")
+//                }
+//
+//
+//            override fun onResponse(
+//                call: Call<List<GitProperty>>,
+//                response: Response<List<GitProperty>>
+//            ) {
+//                _response.value = "Success :${response.body()?.size} git properties retrieved"
+//                //Log.i("onSuccess",response.body())
+//            }
+//        })
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        //if fragment cancelled loading should be stopped
+        viewModelJob.cancel()
     }
 //    override fun onFailure(call: Call<String>, t: Throwable) {
 //        _response.value = "Failure: " + t.message
